2d7c7efe40dc9db5d4213ec5aba3d719fc8e7121
stuetz <t.stuetz@htl-leonding.ac.at>
2022-04-30 19:38:52 +0200

    stuetz - basic codelab - 09 - lazy list

--- basics-codelab/app/src/main/java/at/htl/basiccodelab/MainActivity.kt
+++ basics-codelab/app/src/main/java/at/htl/basiccodelab/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -42,10 +44,12 @@ fun MyApp() {
 
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+private fun Greetings(names: List<String> = List(1000) {"$it"}) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         for (name in names) {
-            Greeting(name = name)
+            items(items = names) {
+            name -> Greeting(name = name)
+            }
         }
     }
 }
