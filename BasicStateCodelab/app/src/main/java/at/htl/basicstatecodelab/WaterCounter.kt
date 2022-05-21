0b64b496196633ac7dd7e08f26e95f0fb0ba7629
stuetz <t.stuetz@htl-leonding.ac.at>
2022-05-21 21:55:30 +0200

    stuetz - 10 / 5 - memory in compose function - remember

--- BasicStateCodelab/app/src/main/java/at/htl/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/at/htl/basicstatecodelab/WaterCounter.kt
@@ -7,13 +7,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        val count: MutableState<Int> = mutableStateOf(0)
+        val count: MutableState<Int> = remember { mutableStateOf(0) }
 
         Text("You've had ${count.value} glasses.")
         Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
